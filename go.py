9aeb068d123ce65750092ede1f7b5646e13d3474
Baraah_Afana <baraahnidal.afana@student.griffith.ie>
2022-12-26 01:39:07 +0000

    removed dockWidget and setup layout to be more presentable

--- go.py
+++ go.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout
 from PyQt6.QtCore import Qt
 from board import Board
 from score_board import ScoreBoard
@@ -7,6 +7,8 @@ class Go(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.board = Board(self)
+        self.scoreBoard = ScoreBoard()
         self.initUI()
 
     def getBoard(self):
@@ -17,48 +19,23 @@ class Go(QMainWindow):
 
     def initUI(self):
         '''initiates application UI'''
-<<<<<<< HEAD
-        self.board = Board(self)
-        self.setCentralWidget(self.board)
-        self.scoreBoard = ScoreBoard()
-        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.scoreBoard)
+
+        main_widget = QWidget()
+        self.setCentralWidget(main_widget)
+
+        self.setStyleSheet("background-color: yellow")
+
+        layout = QHBoxLayout(main_widget)
+        layout.addWidget(self.board)
+        layout.addWidget(self.scoreBoard)
+
         self.scoreBoard.make_connection(self.board)
 
-        self.resize(800, 800)
-=======
-
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-
-        self.board = Board(self)
-        self.scoreBoard = ScoreBoard()
-        # self.setCentralWidget(self.board)
-
-        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.scoreBoard)
-        self.scoreBoard.make_connection(self.board)
-
-        #  TODO maybe not necessary, setting scoreBoard and board inside a main layout
-        central_widget_layout = QHBoxLayout(central_widget)
-        central_widget_layout.addWidget(self.board, 9)
-        central_widget_layout.addWidget(self.scoreBoard, 2)
-
-        # set min siz based on screen size
         screen = self.screen().availableGeometry()
 
-        #  min size is half of screeen size
-        self.setMinimumSize(int(screen.width() * 0.5), int(screen.height() * 0.5))
+        self.setMinimumWidth(int(screen.width() * 0.8))
+        self.setMinimumHeight(int(screen.height() * 0.88))
 
->>>>>>> 54dc38efff70c8075d58f81e409720057bf97d2e
-        self.center()
         self.setWindowTitle('Go')
         self.show()
 
-    def center(self):
-        '''centers the window on the screen'''
-        gr = self.frameGeometry()
-        screen = self.screen().availableGeometry().center()
-
-        gr.moveCenter(screen)
-        self.move(gr.topLeft())
-        #size = self.geometry()
-        #self.move((screen.width() - size.width()) / 2,(screen.height() - size.height()) / 2)
