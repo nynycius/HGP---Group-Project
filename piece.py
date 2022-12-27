4fdab9c7c3b049e7a6021a8bc4e8c23df38ba3ea
Baraah_Afana <baraahnidal.afana@student.griffith.ie>
2022-12-27 17:24:47 +0000

    minor implementation of single capture

--- piece.py
+++ piece.py
@@ -7,19 +7,19 @@ from PyQt6.QtWidgets import QPushButton, QSizePolicy, QApplication
 
 # TODO: Add more functions as needed for your Pieces
 class Piece(QPushButton):
-    NoPiece = 0
-    White = 1
-    Black = 2
-    status = 0  # default to nopiece
-    liberties = 0  # default no liberties
-    x = -1
-    y = -1
-    icon = QIcon()
+    # NoPiece = 0
+    # White = 1
+    # Black = 2
+    # status = 0  # default to nopiece
+    # liberties = 0  # default no liberties
+    # x = -1
+    # y = -1
+    # icon = QIcon()
 
     def __init__(self, board, x, y):  # constructor
         super().__init__()
         self.status = 0
-        self.liberties = 4  # starting with 0 liberty as default, must set right liberty when placed
+        self.liberties = 4  # starting with 4 liberty as default, must set right liberty when placed
         self.x = x
         self.y = y
         #  comment out the next line to see button border
@@ -33,6 +33,7 @@ class Piece(QPushButton):
         self.board = board
 
         self.adjacentPiece = []
+        self.adjacentLiberties = []
 
     # Small test to change the icons, must be changed based on players turn if Status == 0 (blank) only
     def piece_color(self):
@@ -51,7 +52,14 @@ class Piece(QPushButton):
                     self.status = 2
 
             print(f"pressed: {self.getPiece()}, x and y: {self.get_x_and_y()}")
-            print(self.getAdjacentPieces())
+         #   print(self.getAdjacentPieces())
+            for adjacent in self.adjacentPiece:
+                print(str(adjacent.get_x_and_y()), adjacent.getLiberties())
+
+            print(len(self.getOpponentPiece()))
+
+
+
 
     def getPiece(self):  # return PieceType
         return self.status
@@ -61,9 +69,8 @@ class Piece(QPushButton):
         self.setIcon(QIcon("./icons/blank.png"))
 
     def getLiberties(self):  # return Liberties
-
         for adjacent_piece in self.adjacentPiece:
-            if adjacent_piece > 0:
+            if adjacent_piece.getPiece() > 0:
                 self.liberties -= 1
         return self.liberties
 
@@ -85,6 +92,7 @@ class Piece(QPushButton):
     def getAdjacentPieces(self):
         # creating list to store adjacent values
         self.adjacentPiece = []
+
         # creating reference to board piece Array
         pieces_arr = self.board.piecesArray
 
@@ -94,36 +102,64 @@ class Piece(QPushButton):
 
         #  if one of the conditions bellows happens, them the index is out of boundaries,
         if y + 1 != len(pieces_arr):
-            self.adjacentPiece.append(pieces_arr[self.x][self.y + 1].getPiece())
-
+            self.adjacentPiece.append(pieces_arr[self.x][self.y + 1])
         if x + 1 != len(pieces_arr):
-            self.adjacentPiece.append(pieces_arr[self.x + 1][self.y].getPiece())
+            self.adjacentPiece.append(pieces_arr[self.x + 1][self.y])
 
         if y - 1 != -1:
-            self.adjacentPiece.append(pieces_arr[self.x][self.y - 1].getPiece())
+            self.adjacentPiece.append(pieces_arr[self.x][self.y - 1])
 
         if x - 1 != -1:
-            self.adjacentPiece.append(pieces_arr[self.x - 1][self.y].getPiece())
+            self.adjacentPiece.append(pieces_arr[self.x - 1][self.y])
 
         return self.adjacentPiece
 
     def suicide(self, adjacent):  # pass list of adjacent positions to check movement is valid
+    #
+    #     #  set dataStructure doesn't allow repetitive values, so if it len equals to 1, all elements are the same
+    #     same_values = set(adjacent)
+    #     turn = self.board.turn_counter % 2  # add one to match with the current player turn
+    #
+    #     if len(same_values) == 1:
+    #         # if all elements are the same, check if it is 0, 1 or 2
+    #         if (adjacent[0] == 0
+    #                 or adjacent[0] == 1 and turn == 1
+    #                 or adjacent[0] == 2 and turn == 0):
+    #             # if it is 0 the position is valid
+    #             return True
+    #         #  else, the movement is invalid return false
+    #         else:
+    #             print(f"{str(self.get_x_and_y())} The movement is not valid, self capture detected ")
+    #             return False
+    #     else:
+        return True
 
-        #  set dataStructure doesn't allow repetitive values, so if it len equals to 1, all elements are the same
-        same_values = set(adjacent)
-        turn = self.board.turn_counter % 2  # add one to match with the current player turn
+    def getOpponentPiece(self):
+        if self.status == 0:
+            raise Exception("This piece is empty")
 
-        if len(same_values) == 1:
-            # if all elements are the same, check if it is 0, 1 or 2
-            if (adjacent[0] == 0
-                    or adjacent[0] == 1 and turn == 1
-                    or adjacent[0] == 2 and turn == 0):
-                # if it is 0 the position is valid
-                return True
-            #  else, the movement is invalid return false
-            else:
-                print(f"{str(self.get_x_and_y())} The movement is not valid, self capture detected ")
-                return False
+        opponent = []
+        for adjacent in self.adjacentPiece:
+            if adjacent.getPiece() != 0 and adjacent.getPiece != self.status:
+                    opponent.append(adjacent)
+
+        return opponent
 
 
 
+    # def captureSinglePiece(self, opponent):
+    #     enemies_around = 0
+    #     surrounded_by_enemies = False
+    #
+    #     if len(opponent) == 1:
+    #         piece = opponent[0]
+    #         print("efeg",len(piece.getAdjacentPieces()))
+    #         for a in piece.getAdjacentPieces():
+    #             if a.getPiece() != piece.getPiece():
+    #                 enemies_around += 1
+    #
+    #         if enemies_around == len(piece.getAdjacentPieces()):
+    #             surrounded_by_enemies = True
+    #
+    #         if surrounded_by_enemies:
+    #             piece.setPiece(0)
